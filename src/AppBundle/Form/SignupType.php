5a2c354bdd2e9f6f958160e8f4b80bfb2cfe62f4
apple <1>
2017-04-15 08:31:39 +0300

    Signup form in progress...

--- src/AppBundle/Form/SignupType.php
+++ src/AppBundle/Form/SignupType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -25,7 +26,8 @@ class SignupType extends AbstractType
                     'first_options'  => ['label' => 'Password', 'attr' => ['placeholder' => 'Password...']],
                     'second_options' => ['label' => 'Repeat Password', 'attr' => ['placeholder' => 'Repeat Password...']],
                 ]
-            );
+            )
+            ->add('signup', SubmitType::class, ['label' => 'Create My Account', 'attr' => ['class' => 'button expand']]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
